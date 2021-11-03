b6d3f5ea4545beb223446458466fcd92a0486396
Richard Li <richardli485@gmail.com>
2021-11-03 12:36:04 -0400

    camera grayscale works!

--- camerapro.py
+++ camerapro.py
@@ -1,32 +1,17 @@
 #define a class
 
 import numpy as np
-from cv2 import *
+import cv2
 
 
 # initialize the camera
 def camcapture():
-    cam = VideoCapture(0)   # 0 -> index of camera
+    cam = cv2.VideoCapture(0)   # 0 -> index of camera
     s, img = cam.read()
 
     if s:    # frame captured without any errors
-        namedWindow("cam-test")
-        imshow("cam-test",img)
-        waitKey(0)
-        destroyWindow("cam-test")  
-
-
-def grayscale(rgb):
-    
-    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
-    gscale = 0.2989 * r + 0.5870 * g + 0.1140 * b
-
-    return gscale
-
-#return grayscale picture
-
-def gscalepic():
-    return grayscale(camcapture())
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return gray      
 
 
 
