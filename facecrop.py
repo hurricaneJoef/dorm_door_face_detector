f23d9b61b062e45e0a90ffbd219561910fcbacb1
hurricaneJoef <33767776+hurricaneJoef@users.noreply.github.com>
2021-11-03 12:45:03 -0400

    Update facecrop.py

--- facecrop.py
+++ facecrop.py
@@ -4,4 +4,18 @@ import numpy as np
 import cv2
 def crop2face(gimg):
     #crop input image to largest face
-
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+    
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gimg, 1.1, 4)
+    
+    # Draw rectangle around the faces and crop the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        faces = img[y:y + h, x:x + w]
+        cv2.imshow("face",faces)
+        
+    # Display the output
+    
+    cv2.imshow('img', img)
+    cv2.waitKey()
