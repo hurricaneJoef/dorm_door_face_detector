1c57e03a82edb81f0b50d6935f170ea578c800e0
hurricaneJoef <33767776+hurricaneJoef@users.noreply.github.com>
2021-11-03 12:03:30 -0400

    updated state loop and fixed some bugs

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
 # calling camera function
 # calling face/noface function -- if noface, return to camera
 # calling identify function
+class statefacerec:
 
-camera()
-
-face = isface()
-
-
-def stateloop(self):
-    print("state: " + str(self.state))
     
-    #starts the state machine
-    self.state = 0
+
+
+    def stateloop(self):
+        print("state: " + str(self.state))
     
-    #camera 
-    if self.state == 0:
+        #starts the state machine
+        self.state = 0
+    
+        #camera 
+        if self.state == 0:
+            pic = camera()
+            if pic:
+                self.state = 1
         #take a picture!
     
-    #isface?
-    elif self.state == 1:
-        # face will return (face_img, self.state)
-        if face_img == False:
-            self.state = 0
-        else self.state = 2  
+        #isface?
+        elif self.state == 1:
+            # face will return (face_img, self.state)
+            face = isface()
+            if face_img == False:
+                self.state = 0
+            else self.state = 2  
     
-    #if face is too far -- step 1.1
-    elif self.state == 1.1:
-        print("too far")
+        #if face is too far -- step 1.1
+        elif self.state == 1.1:
+            print("too far")
         
-    #isme?   
-    elif self.state == 2:
-        #if it is joseph
-        if isme:
-            print("hello, joseph")
-        #if it is not joseph
+        #isme?   
+        elif self.state == 2:
+            #if it is joseph
+            if isme:
+                print("hello, joseph")
+            #if it is not joseph
+            else:
+                print ("hi! I don't know you")
+        
+    
+        elif self.state == 3:
+        
         else:
-            print ("hi! I don't know you")
-        
-    
-    elif self.state == 3:
-        
-    else:
-        print("how the fuck did you get here?")
+            print("how the fuck did you get here?")
+if __name__ == "__main__":
+    stateloop()
