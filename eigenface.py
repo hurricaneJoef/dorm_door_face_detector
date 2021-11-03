a0a487a565d5a2b750fd0fc476fc01186a4fd146
Richard Li <richardli485@gmail.com>
2021-11-03 12:57:29 -0400

    imported all scikitlearn dependencies

--- eigenface.py
+++ eigenface.py
@@ -3,9 +3,13 @@ from facecrop import *
 
 import numpy as np 
 import pandas as pd 
-import matplotlib.pyplot as pltfrom sklearn.svm import SVC
-import sklearn
+import matplotlib.pyplot as pltfrom 
+
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
-from sklearn.metrics import confusion_matrix, classification_reportimport warnings
-warnings.filterwarnings(‘ignore’)
+from sklearn.metrics import confusion_matrix, classification_report
+
+import warnings
+warnings.filterwarnings('ignore')
+
--- main.py
+++ main.py
@@ -5,8 +5,12 @@
 from facecrop import *
 from camerapro import *
 class statefacerec:
+
+    
+
+
     def stateloop(self):
-        print("state: " + str(self.state))   
+        print("state: " + str(self.state))     
     
         #starts the state machine
         self.state = 0
