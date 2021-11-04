19c5ebe93d62afe09ef57959ea13731e3a962c85
hurricaneJoef <33767776+hurricaneJoef@users.noreply.github.com>
2021-11-04 16:00:49 -0400

    training files added

--- camerapro.py
+++ camerapro.py
@@ -7,10 +7,15 @@ import cv2
 # initialize the camera
 def camcapture():
     cam = cv2.VideoCapture(0)   # 0 -> index of camera
+    for i in range(10):
+        s, img = cam.read()
     s, img = cam.read()
 
     if s:    # frame captured without any errors
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.normalize(img, img, 0, 255, cv2.NORM_MINMAX)
+
+
         return gray
 
 
--- test.py
+++ test.py
@@ -1,16 +1,42 @@
 from camerapro import *
-from facecrop import *
+from facecrop import * 
+import numpy as np
+import cv2 as cv
+import os.path
 
 
 img = camcapture()
 cv2.namedWindow("cam-test")
 cv2.imshow("cam-test",img)
 
+try:
+    with open('trainedfaced.npy', 'rb') as f:
+        facestrained =  np.load(f)#subject id
+except FileNotFoundError:
+    facestrained = 0
+
+try:
+    os.mkdir('/trainingdata/img/cropped')
+except Exception:
+    pass
 
 faces = crop2face(img)
-cv2.namedWindow("face-test")
-cv2.imshow("face-test",faces)
+if faces != None:
+    cv2.namedWindow("face-test")
+    cv2.imshow("face-test",faces)
+    cv2.imwrite(os.path.join('trainingdata','img','cropped'+str(facestrained)+'.png'),faces)
+
+cv2.imwrite(os.path.join('trainingdata','img'+str(facestrained)+'.png'),img)
+
+facestrained+=1
+with open('trainedfaced.npy', 'wb') as f:
+    np.save(f,facestrained )#subject id
+
+
 cv2.waitKey(0)
 cv2.destroyWindow("cam-test") 
-cv2.waitKey(0)
-cv2.destroyWindow("face-test") 
+try:
+    cv2.destroyWindow("face-test")
+except Exception:
+    None
+print(facestrained)
