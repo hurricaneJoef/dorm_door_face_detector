07bc19054ba157a3249155e242b35405bdf704be
hurricaneJoef <33767776+hurricaneJoef@users.noreply.github.com>
2021-11-07 14:42:44 -0500

    added faces

--- eigenface.py
+++ eigenface.py
@@ -24,26 +24,40 @@ def isme(img):
         mytimg = cv2.imread(os.path.join('correctfaces', filename))
         
         meis = cv2.cvtColor(cv2.resize(mytimg, dim, interpolation = cv2.INTER_AREA), cv2.COLOR_BGR2GRAY)#resize
-        print(meis.shape)
+        #print(meis.shape)
         try:
             correcteigs = np.append(correcteigs,np.ubyte(meis.reshape((meis.size,1))),1)
         except Exception:
             correcteigs = np.ubyte(meis.reshape((meis.size,1)))
     mcorrecteigs = correcteigs               -eigenmean
-    cvec = np.linalg.lstsq(mcorrecteigs,eigenvectors.T)[0].T
-    print(cvec.shape)
+    #print(mcorrecteigs.shape)
+    cvec,cresid,crank,cs = np.linalg.lstsq(eigenvectors.T,mcorrecteigs)
+    #print(cvec.shape)
 
     bruhimg = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)#resize
     timg  = bruhimg.reshape((bruhimg.size,1))         -eigenmean
     tarray = np.ubyte(timg.reshape((timg.size,1)))
-    tvec = np.linalg.lstsq(timg,eigenvectors.T)[0].T
-    print(tvec.shape)
-
+    tvec,tresid,trank,ts = np.linalg.lstsq(eigenvectors.T,timg)
+    #print(tvec.shape)
+    i=0
     for column in cvec.T:
-        distsq = np.sum(np.square(tvec-column))
+        
+        distsq = np.sum(np.square(tvec-column.T.reshape(column.size,1)))
+        #print("lillian was here")
+        #print(column.T.shape)
         print(distsq)
-        if distsq < 0.00000005:
+        if np.min(distsq) < 40000:
+            '''cimg = np.multiply(column,eigenvectors.T).resize((64,64))
+            cv2.namedWindow("cvec-test")
+            cv2.imshow("cvec-test",cimg)
+            cv2.waitKey(0)
+            cv2.namedWindow("tvec-test")
+            cv2.imshow("tvec-test",bruhimg)
+            cv2.waitKey(0)
+            cv2.destroyWindow("tvec-test")
+            cv2.destroyWindow("cvec-test")  #'''
             return True
+        i += 1
     return False
             
     
@@ -56,11 +70,21 @@ if __name__ == "__main__":
         img = camcapture()
         face = crop2face(img)
         None#'''r
-    face = cv2.cvtColor( cv2.imread("trainingdata/cropped/img5.png") , cv2.COLOR_BGR2GRAY)
+    
     cv2.namedWindow("isme-test")
     cv2.imshow("isme-test",face)
     cv2.waitKey(0)
     cv2.destroyWindow("isme-test") 
     output = isme(face)
     print("output")
-    print(output) 
+    print(output)
+    for filename in os.listdir('trainingdata/cropped'):
+        face = cv2.cvtColor( cv2.imread("trainingdata/cropped/"+filename) , cv2.COLOR_BGR2GRAY)
+        print(filename)
+        cv2.namedWindow("isme-test")
+        cv2.imshow("isme-test",face)
+        cv2.waitKey(0)
+        cv2.destroyWindow("isme-test") 
+        output = isme(face)
+        print("output")
+        print(output)
